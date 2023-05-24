43ca091b236debbdefe4959f867390da429ebd7b
devansh698 <122423665+devansh698@users.noreply.github.com>
2023-05-24 12:43:37 +0530

    new

---
+++ img1/login.js
@@ -0,0 +1,111 @@
+"use strict";
+//login
+const loginBtn = document.querySelector("#login-btn");
+const signUpBtn = document.querySelector("#sign-up-btn");
+const usernameIn = document.querySelector("#userid");
+const emailIn = document.querySelector("#emailid");
+const usernameOut = document.querySelector("#username-display");
+const usernameOutNavBar = document.querySelector(".sign-in");
+const signUpNavBar = document.querySelector(".sign-up");
+// const slashNavBar = document.querySelector("#slash");
+const passwordIn = document.querySelector("#pswrd");
+const loginForm = document.querySelector("#login-form");
+
+function checkUsername() {
+  let password = passwordIn.value;
+  if (usernameIn.value === "") {
+    usernameOut.innerHTML = "Please enter a username.";
+  } else if (password.length < 8) {
+    usernameOut.innerHTML =
+      "Please enter a password that is at least 8 character long.";
+  } else {
+    Storage.setUsername(usernameIn.value);
+    location.reload();
+    if (usernameIn.value == "E-commerce") {
+      window.open("https://lchua2314.github.io/E-commerce-Website/index.html");
+      alert("Thanks for looking at the code! :)");
+    }
+  }
+}
+
+function createNewUsername() {
+  let password = passwordIn.value,
+    email = emailIn.value;
+  if (!validateEmail(email)) {
+    usernameOut.innerHTML = "Please enter a valid email address.";
+  } else if (usernameIn.value === "") {
+    usernameOut.innerHTML = "Please enter a username.";
+  } else if (password.length < 8) {
+    usernameOut.innerHTML =
+      "Please enter a password that is at least 8 character long.";
+  } else {
+    Storage.setUsername(usernameIn.value);
+    location.reload();
+  }
+}
+
+function validateEmail(inputEmail) {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(String(inputEmail).toLowerCase());
+}
+class Storage {
+  static setUsername(inputUsername) {
+    localStorage.setItem("username", inputUsername);
+  }
+  static getUsername() {
+    return localStorage.getItem("username");
+  }
+  static setPassword(inputPassword) {
+    localStorage.setItem("password", inputPassword);
+  }
+  static getPassword() {
+    return localStorage.getItem("password");
+  }
+}
+let currUser = Storage.getUsername();
+if (currUser) {
+  usernameOutNavBar.innerHTML = '<i class="fas fa-user"></i> ' + currUser;
+  signUpNavBar.innerHTML = "";
+  // slashNavBar.innerHTML = "";
+  if (loginForm) {
+    loginForm.innerHTML = "";
+    usernameOut.innerHTML =
+      'Currently logged in as: <br> <i class="fas fa-user"></i> ' + currUser;
+  }
+}
+
+if (loginBtn) {
+  if (!currUser) {
+    loginBtn.addEventListener("click", checkUsername);
+    loginForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+    });
+  } else {
+    loginBtn.innerHTML = "Sign Out";
+    loginBtn.addEventListener("click", () => {
+      localStorage.removeItem("username");
+      location.reload();
+    });
+    loginForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+    });
+  }
+}
+
+if (signUpBtn) {
+  if (!currUser) {
+    signUpBtn.addEventListener("click", createNewUsername);
+    loginForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+    });
+  } else {
+    signUpBtn.innerHTML = "Sign Out";
+    signUpBtn.addEventListener("click", () => {
+      localStorage.removeItem("username");
+      location.reload();
+    });
+    loginForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+    });
+  }
+}
